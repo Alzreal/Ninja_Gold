c5da649ea87d2546669ec80b017d8502e68c9f28
Christopher Farrell <Irishmedic250@gmail.com>
2021-10-19 21:37:30 -0400

    Building functions

--- Ninja_rich/urls.py
+++ Ninja_rich/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('process_money', views.process_money),
 ]
--- Ninja_rich/views.py
+++ Ninja_rich/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render, redirect
+import random
 
 def index(request):
     return render (request, "index.html")
+
+def process_money(request):
+    name=request.POST
+    if name == 'farm':
+        gold=random.randint(10, 20)
+        context= {
+            'gold':gold
+        }
+    return render (request, 'index.html', context)

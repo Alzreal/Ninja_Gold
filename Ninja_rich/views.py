a2c456bd3264b504fbe7f075d5e77e70497877ab
Christopher Farrell <Irishmedic250@gmail.com>
2021-10-20 19:56:23 -0400

    Working on Process_money

--- Ninja_rich/views.py
+++ Ninja_rich/views.py
@@ -5,9 +5,23 @@ def index(request):
     return render (request, "index.html")
 
 def process_money(request):
-    name=request.POST
-    if name == 'farm':
+    if 'farm' in request.POST:
         gold=random.randint(10, 20)
+        context= {
+            'gold':gold
+        }
+    if 'cave' in request.POST:
+        gold=random.randint(5, 10)
+        context= {
+            'gold':gold
+        }
+    if 'house' in request.POST:
+        gold=random.randint(2, 5)
+        context= {
+            'gold':gold
+        }
+    if 'casino' in request.POST:
+        gold=random.randint(-50, 50)
         context= {
             'gold':gold
         }

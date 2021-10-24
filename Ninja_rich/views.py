e81e818b106ec458dd02363b7317a52ff540e791
Christopher Farrell <Irishmedic250@gmail.com>
2021-10-23 21:03:34 -0400

    More work on process_money
    Now have [log] and working as intended.

--- Ninja_rich/views.py
+++ Ninja_rich/views.py
@@ -3,34 +3,29 @@ from time import localtime, strftime
 import random
 
 def index(request):
-    context = {
-        'date': strftime("%B %d, %Y - %H:%M:%S %p", localtime()),
-    }
-    return render (request, "index.html", context)
+    return render (request, "index.html")
 
 def process_money(request):
     if "get_gold" not in request.session:
-        request.session['get_gold']=[]
-    if 'farm' in request.POST:
-        gold=random.randint(10, 20)
-        request.session['get_gold']+=gold
-    
-    if "get_gold" not in request.session:
-        request.session['get_gold']=[]
-    if 'cave' in request.POST:
-        gold=random.randint(5, 10)
-        request.session['get_gold']+=gold
-    
-    if "get_gold" not in request.session:
-        request.session['get_gold']=[]
-    if 'house' in request.POST:
-        gold=random.randint(2, 5)
-        request.session['get_gold']+=gold
-    
-    if "get_gold" not in request.session:
-        request.session['get_gold']=[]
-    if 'casino' in request.POST:
-        gold=random.randint(-50, 50)
-        request.session['get_gold']+=gold
-    
+        request.session['get_gold']=0
+        request.session['log']=[]
+    else:
+        request.session['date']= strftime("%B %d, %Y - %H:%M:%S %p", localtime())
+        if 'farm' in request.POST:
+            request.session['gold']=random.randint(10, 20)
+            request.session['get_gold']+=request.session['gold']
+            request.session['log'].append(
+                f"Your ninja recieved {request.session['gold']} gold from the farm on {request.session['date']}"
+            )
+        if 'cave' in request.POST:
+            request.session['gold']=random.randint(5, 10)
+            request.session['get_gold']+=request.session['gold']
+        if 'house' in request.POST:
+            request.session['gold']=random.randint(2, 5)
+            request.session['get_gold']+=request.session['gold']
+        if 'casino' in request.POST:
+            request.session['gold']=random.randint(-50, 50)
+            request.session['get_gold']+=request.session['gold']
+        
+
     return redirect('/')

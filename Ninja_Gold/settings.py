f06ab99b9dd94f41205b4f366ccdbdf54cc75391
Chris <Irishmedic250@gmail.com>
2022-04-07 14:28:57 -0400

    mods for deplyment

--- Ninja_Gold/settings.py
+++ Ninja_Gold/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(fs%v5w&$0h2uk5rz+0#g-!h58cw=xri6r6et1*k+8yy_$n!87'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
